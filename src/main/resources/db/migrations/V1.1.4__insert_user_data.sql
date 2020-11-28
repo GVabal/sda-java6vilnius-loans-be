84cf6480d6c7863f9a6c36b0c52b2333e6fa32c2
GVabal <gvabal@gmail.com>
2020-11-28 12:31:23 +0200

    Adjust initial data

--- src/main/resources/db/migrations/V1.1.4__insert_user_data.sql
+++ src/main/resources/db/migrations/V1.1.4__insert_user_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO user(username, email, password, status, role_id)
-VALUES  ('customer@email.com', 'user@email.com', '$2a$14$NHYTZh3j21GwXKSX0w9qm.0.JN1Su.xxVzTVkhcqGAtWx2qf4q3v6', 'ACTIVE', 2),
+VALUES  ('customer@email.com', 'customer@email.com', '$2a$14$NHYTZh3j21GwXKSX0w9qm.0.JN1Su.xxVzTVkhcqGAtWx2qf4q3v6', 'ACTIVE', 2),
         ('admin@email.com', 'admin@email.com', '$2a$14$F/qc3QxlActLGY9nTpyw7OORfoSxhc7gjXAQPW/GeJFge.k16xm0C', 'ACTIVE', 1),
         ('employee@email.com', 'employee@email.com', '$2a$14$gC7HQuQrbwsCVGoQU79wbOdhfzPOT8uFpZ6MbuDxGIyWl1kgr57Iu', 'ACTIVE', 3);
