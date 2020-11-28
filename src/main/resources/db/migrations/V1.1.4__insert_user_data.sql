75a95f5aacfb937b35bcd172a7663a62986f44d7
GVabal <gvabal@gmail.com>
2020-11-28 12:25:19 +0200

    Adjust initial data

--- src/main/resources/db/migrations/V1.1.4__insert_user_data.sql
+++ src/main/resources/db/migrations/V1.1.4__insert_user_data.sql
@@ -1,5 +1,4 @@
 INSERT INTO user(username, email, password, status, role_id)
-VALUES  ('user@email.com', 'user@email.com', '$2a$14$NHYTZh3j21GwXKSX0w9qm.0.JN1Su.xxVzTVkhcqGAtWx2qf4q3v6', 'ACTIVE', 2),
+VALUES  ('customer@email.com', 'user@email.com', '$2a$14$NHYTZh3j21GwXKSX0w9qm.0.JN1Su.xxVzTVkhcqGAtWx2qf4q3v6', 'ACTIVE', 2),
         ('admin@email.com', 'admin@email.com', '$2a$14$F/qc3QxlActLGY9nTpyw7OORfoSxhc7gjXAQPW/GeJFge.k16xm0C', 'ACTIVE', 1),
-        ('employee@email.com', 'employee@email.com', '$2a$14$gC7HQuQrbwsCVGoQU79wbOdhfzPOT8uFpZ6MbuDxGIyWl1kgr57Iu', 'ACTIVE', 3),
-        ('otherUser@email.com', 'otherUser@email.com', '$2a$11$x02tDIDM8qLcXYJIyV0MwuqDtSpfhO/Vt3guHZKE7k.T25nIquqAa', 'ACTIVE', 2);
+        ('employee@email.com', 'employee@email.com', '$2a$14$gC7HQuQrbwsCVGoQU79wbOdhfzPOT8uFpZ6MbuDxGIyWl1kgr57Iu', 'ACTIVE', 3);
--- src/main/resources/db/migrations/V1.2.2__insert_customer_data.sql
+++ src/main/resources/db/migrations/V1.2.2__insert_customer_data.sql
@@ -1,3 +1,2 @@
 INSERT INTO customer(id, first_name, last_name, email, bank_account_number)
-VALUES  (1, 'Vardenis', 'Pavardenis', 'user@email.com', 'LT121000011101001000'),
-        (4, 'Jonas', 'Jonaitis', 'otherUser@email.com', 'LT222000022202002000');
+VALUES  (1, 'Vardenis', 'Pavardenis', 'customer@email.com', 'LT121000011101001000');
